9d75248b0a30cf36fa46475c703ba966109aee9e
walinchus <Lucia@professionalnewsservices.com>
2017-05-30 13:05:46 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,40 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#import our libraries
+import scraperwiki
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# create a new function, which gets passed a variable we're going to call 'url'
+def scrape_ccg(url):
+    html = scraperwiki.scrape(url)
+    print html
+    root = lxml.html.fromstring(html)
+    #line below selects all <div class="reveal-modal medium"> - note that because there is a space in the value of the div class, we need to use a space to indicate that
+    rows = root.cssselect("div.reveal-modal.medium") 
+    for row in rows:
+        print row
+        # Set up our data record - we'll need it later
+        record = {}
+        h2s = row.cssselect("h2") #grab all <h2> tags within our <div>
+        membername = h2s[0].text
+        #repeat process for <p class="lead"> 
+        leads = row.cssselect("p.lead")
+        membertitle = leads[0].text
+        #repeat process for <p>
+        ps = row.cssselect("p")
+        #this line puts the contents of the last <p tag by using [-1]
+        memberbiog = ps[-1].text_content()
+        record['URL'] = url
+        record['Name'] = membername
+        record['Title'] = membertitle
+        record['Description'] = memberbiog
+        print record, '------------'
+        # Finally, save the record to the datastore - 'Name' is our unique key
+        scraperwiki.sqlite.save(["Name"], record)
+        
+#list of URLs with similar CMS compiled with this advanced search on Google: site:nhs.uk inurl:about-us/our-governing-body.aspx
+ccglist = ['www.hounslowccg.nhs.uk/',  'www.centrallondonccg.nhs.uk/', 'www.hammersmithfulhamccg.nhs.uk/']
+#'www.ealingccg.nhs.uk/' has similar page but at different URL: http://www.hammersmithfulhamccg.nhs.uk/about-us/our-governing-body.aspx
+for ccg in ccglist:
+    fullurl = 'http://'+ccg+'about-us/our-governing-body.aspx'
+    print 'scraping ', fullurl
+    scrape_ccg(fullurl)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
