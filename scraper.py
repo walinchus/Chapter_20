a43ef437a1784783bc89e0923d0cbef30f880918
walinchus <Lucia@professionalnewsservices.com>
2017-05-30 13:18:44 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -30,6 +30,29 @@ def scrape_ccg(url):
         # Finally, save the record to the datastore - 'Name' is our unique key
         scraperwiki.sqlite.save(["Name"], record)
         
+        rows = root.cssselect("div.reveal-modal.medium") 
+    for row in rows:
+        print row
+        # Set up our data record - we'll need it later
+        record = {}
+        h2s = row.cssselect("h2") #grab all <h2> tags within our <div>
+        membername = h2s[0].text
+        #repeat process for <p class="lead"> 
+        leads = row.cssselect("p.lead")
+        membertitle = leads[0].text
+        #repeat process for <p>
+        ps = row.cssselect("p")
+        #this line puts the contents of the last <p tag by using [-1]
+        memberbiog = ps[-1].text_content()
+        record['URL'] = url
+        record['Name'] = membername
+        record['Title'] = membertitle
+        record['Description'] = memberbiog
+        print record, '------------'
+        # Finally, save the record to the datastore - 'Name' is our unique key
+        scraperwiki.sqlite.save(["Name"], record)
+        
+        
 #list of URLs with similar CMS compiled with this advanced search on Google: site:nhs.uk inurl:about-us/our-governing-body.aspx
 ccglist = ['www.hounslowccg.nhs.uk/',  'www.centrallondonccg.nhs.uk/', 'www.hammersmithfulhamccg.nhs.uk/']
 #'www.ealingccg.nhs.uk/' has similar page but at different URL: http://www.hammersmithfulhamccg.nhs.uk/about-us/our-governing-body.aspx
